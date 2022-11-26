6f399ba6936e856896a754735c1c6349ab70dc28
Ken Shibata <nyiyui@nyiyui.ca>
2022-11-25 23:41:52 -0500

    nix and stuff

--- scavenger2022/shell.nix
+++ scavenger2022/shell.nix
@@ -9,4 +9,13 @@ let
 in
   pkgs.mkShell {
     packages = [ pythonWithPkgs ];
+
+    shellHook = ''
+      export PIP_PREFIX=$(pwd)/.build/pip_packages
+      export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+      export PATH="$PIP_PREFIX/bin:$PATH"
+      unset SOURCE_DATE_EPOCH
+      python3 -m pip install pipenv
+      pipenv install --skip-lock
+    '';
   }
