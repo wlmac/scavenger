6ba5b93fbeec897ba76fdae380c113a734d1a911
Ken Shibata <nyiyui@nyiyui.ca>
2022-11-25 22:44:34 -0500

    im dumb kms

--- scavenger2022/core/urls.py
+++ scavenger2022/core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/', include('core.api.urls')),
-    path('login/', views.login, name='login'),
-    path('auth/', views.auth, name='auth'),
+    path('login/', views.oauth_login, name='oauth_login'),
+    path('auth/', views.oauth_auth, name='oauth_auth'),
     path('logout/', views.logout, name='logout'),
 ]
