97b86c84d0096a1d151e9d9c55c5271fc7330bb5
Ken Shibata <nyiyui@nyiyui.ca>
2022-11-25 23:11:58 -0500

    cleanup

--- scavenger2022/scavenger2022/local_settings.py
+++
@@ -1,8 +0,0 @@
-base_url = 'https://maclyonsden.com'
-YASOI = dict(
-    client_id='v5fcNotjcRoKahkpbYQgWWk5OszarR6sljCOJX56',
-    client_secret='GfNPtIKee7jwMCjDZb8IZkKQCl21FOWT8sVPM7zTqJsfYO8ZTXvoUgzTGfoXcbnd33JjjwvGx458eEdykyhpQ1P7EFGBThdBLZFH8ayF2JI4lnZ6MGKSSB3g',
-    authorize_url=f'{base_url}/authorize',
-    token_url=f'{base_url}/token/',
-    me_url=f'{base_url}/api/me',
-)
