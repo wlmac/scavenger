a327ad07eb609b93bf3dfa23f12d8ffc3e5472ba
Ken Shibata <nyiyui@nyiyui.ca>
2022-11-25 22:43:23 -0500

    why you do this git
    or maybe im really dumb at using git

--- scavenger2022/core/models.py
+++ scavenger2022/core/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-<<<<<<< HEAD
-=======
 
->>>>>>> 9eed3de62b1ad86786086ac57fd0d42cfda51453
 
 class User(AbstractUser):
     metropolis_id = models.IntegerField()
@@ -22,17 +19,8 @@ class QrCode(models.Model):
 
 
 class Hint(models.Model):
-<<<<<<< HEAD
-=======
-    id = models.AutoField(primary_key=True)
->>>>>>> 9eed3de62b1ad86786086ac57fd0d42cfda51453
     qr_code = models.ForeignKey(QrCode, on_delete=models.CASCADE)
     hint = models.CharField(max_length=100)
 
     def __str__(self):
-<<<<<<< HEAD
-        return self.qr_code
-
-=======
         return self.id
->>>>>>> 9eed3de62b1ad86786086ac57fd0d42cfda51453
