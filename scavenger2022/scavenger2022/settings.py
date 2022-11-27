541bf29da0c3af44df5f7f0de70d23720bf8db95
nyiyui <+@nyiyui.ca>
2022-11-27 00:31:33 -0500

    reformat

--- scavenger2022/scavenger2022/settings.py
+++ scavenger2022/scavenger2022/settings.py
@@ -110,9 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (
-    ('static', BASE_DIR / 'core/static/core'),
-)
+STATICFILES_DIRS = (("static", BASE_DIR / "core/static/core"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
