7b0a34c190664041361f8f6c1ef3b4227460e22a
nyiyui <+@nyiyui.ca>
2022-12-06 00:15:33 -0500

    bugfix: fix NameError for QrCode

--- scavenger2022/core/views/qr.py
+++ scavenger2022/core/views/qr.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.utils.translation import gettext as _
+from ..models import QrCode
 
 
 def team_required(f):
