8f059a7b0882da3b382a3414e1e49d0cc4c85113
nyiyui <+@nyiyui.ca>
2022-12-14 08:55:33 -0500

    index: login required

--- scavenger2022/core/views/index.py
+++ scavenger2022/core/views/index.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 
 
+@login_required
 @require_http_methods(["GET"])
 def index(q):
     return render(q, "core/index.html", {})
