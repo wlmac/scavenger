22e80302f76ce1e3936c623fb194b3f167cc0110
Jason <jasoncameron.all@gmail.com>
2023-12-03 00:42:47 -0500

    time sensitive + cleaner

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,15 +2,17 @@ import datetime
 
 from core.models import Hunt
 from django.conf import settings
+from django.utils import timezone
 
 
 def start(request):
     hunt = Hunt.current_hunt()
+    now = timezone.now()
     return dict(
         START=(start := hunt.start),
-        START_BEFORE=start > datetime.datetime.utcnow(),
-        START_UNTIL=start - datetime.datetime.utcnow(),
+        START_BEFORE=start > now,
+        START_UNTIL=start - now,
         END=(end := hunt.end),
-        END_BEFORE=end > datetime.datetime.utcnow(),
-        END_UNTIL=end - datetime.datetime.utcnow(),
+        END_BEFORE=end > now,
+        END_UNTIL=end - now,
     )
