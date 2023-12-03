0b49c39af5df9400c32db42bdee26c8f5a10ceac
Jason <jasoncameron.all@gmail.com>
2023-12-02 23:45:46 -0500

    Added start/end via current hunt

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,14 +1,16 @@
 import datetime
 
+from core.models import Hunt
 from django.conf import settings
 
 
 def start(request):
-    return dict(
-        START=(start := settings.START),
-        START_BEFORE=start > datetime.datetime.utcnow(),
-        START_UNTIL=start - datetime.datetime.utcnow(),
-        END=(end := settings.END),
-        END_BEFORE=end > datetime.datetime.utcnow(),
-        END_UNTIL=end - datetime.datetime.utcnow(),
-    )
+	hunt = Hunt.current_hunt()
+	return dict(
+		START=(start := hunt.start),
+		START_BEFORE=start > datetime.datetime.utcnow(),
+		START_UNTIL=start - datetime.datetime.utcnow(),
+		END=(end := hunt.end),
+		END_BEFORE=end > datetime.datetime.utcnow(),
+		END_UNTIL=end - datetime.datetime.utcnow(),
+	)
