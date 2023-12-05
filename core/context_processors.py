cba3ac0f566fcd0817503be6d6e2eea3075f10ba
Jason Cameron <git@jasoncameron.dev>
2023-12-04 20:06:03 -0500

    Updates context_processors to support all ranges of events (will refactor later)

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,12 +1,33 @@
 import datetime
 
-from core.models import Hunt
+from django.contrib import messages
+from django.db.models import Func, F, DurationField, Case, DateTimeField, When
 from django.utils import timezone
 
+from core.models import Hunt
 
-def start(request):
+
+def start(request) -> dict:
+    """
+    returns info about the current/closest hunt
+    return dict:
+        START: start time of the current/closest hunt
+        START_BEFORE: bool, if the current time is before the start time
+        START_UNTIL: timedelta, time until the start time
+        END: end time of the current/closest hunt
+        END_BEFORE: bool, if the current time is before the end time
+        END_UNTIL: timedelta, time until the end time
+    """
     now = timezone.now()
-    if (hunt := Hunt.current_hunt()) is not None:
+    if Hunt.objects.count() == 0:
+        messages.add_message(
+            request,
+            messages.WARNING,
+            "No hunts are in the database. Please contact an admin to add a hunt to the database",
+        )
+        event_start = now + datetime.timedelta(weeks=75)
+        event_end = event_start + datetime.timedelta(hours=3)
+    elif (hunt := Hunt.current_hunt()) is not None:
         event_start = hunt.start
         event_end = hunt.end
     elif Hunt.next_hunt() is not None:
@@ -14,11 +35,23 @@ def start(request):
         event_start = hunt.start
         event_end = hunt.end
     else:
-        print("WARNING: No hunt is scheduled")
-        print("Please add a hunt in the future to the database")
-        event_start = now + datetime.timedelta(weeks=75)
-        event_end = event_start + datetime.timedelta(hours=3)
-
+        # at least one
+        closest_hunt = Hunt.objects.annotate(
+            selected_time=Case( # if we should use the start or end time (if it's in the future or past)
+                When(start__lt=now, then=F('end')),
+                default=F('start'),
+                output_field=DateTimeField(),
+            )
+        ).annotate(
+            time_difference=Func(
+                F('selected_time') - now,
+                function='ABS',
+                output_field=DurationField(),
+                )
+        ).order_by('time_difference').first()
+        event_start = closest_hunt.start
+        event_end = closest_hunt.end
+    
     return dict(
         START=event_start,
         START_BEFORE=event_start > now,
