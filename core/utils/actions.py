556a739dd8eb4938cdb3cdd0d4b5f85911ae23b4
Jason Cameron <git@jasoncameron.dev>
2023-12-06 22:18:40 -0500

    updated actions to allow ppl in those groups to access

--- core/utils/actions.py
+++ core/utils/actions.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.auth.models import Group
 from django.db.models import QuerySet
 from django.utils.translation import gettext_lazy as _
@@ -7,15 +7,35 @@ from django.utils.translation import gettext_lazy as _
 from core.models import User
 
 
+def set_group_membership(
+    modeladmin, request, queryset: QuerySet[User], group_name, add=True
+):
+    try:
+        group = Group.objects.get(name=group_name)
+    except Group.DoesNotExist:
+        messages.error(request, f"Group '{group_name}' does not exist.")
+        return
+
+    for user in queryset:
+        if add:
+            user.is_staff = True
+            user.save()
+            user.groups.add(group)
+        else:
+            user.groups.remove(group)
+            if user.groups.count() == 0:
+                user.is_staff = False
+                user.save()
+
+
 @admin.action(
     permissions=["change"],
     description=_("Set selected users as a Logic Puzzle Setter"),
 )
 def set_as_logic_setter(modeladmin, request, queryset: QuerySet[User]):
-    for user in queryset:
-        user.groups.add(
-            Group.objects.get_or_create(name=settings.LOGIC_PUZZLE_SETTERS_NAME)
-        )
+    set_group_membership(
+        modeladmin, request, queryset, settings.LOGIC_PUZZLE_SETTERS_NAME, add=True
+    )
 
 
 @admin.action(
@@ -23,8 +43,9 @@ def set_as_logic_setter(modeladmin, request, queryset: QuerySet[User]):
     description=_("Remove selected users as a Logic Puzzle Setter"),
 )
 def remove_as_logic_setter(modeladmin, request, queryset: QuerySet[User]):
-    for user in queryset:
-        user.groups.remove(Group.objects.get(name=settings.LOGIC_PUZZLE_SETTERS_NAME))
+    set_group_membership(
+        modeladmin, request, queryset, settings.LOGIC_PUZZLE_SETTERS_NAME, add=False
+    )
 
 
 @admin.action(
@@ -32,10 +53,9 @@ def remove_as_logic_setter(modeladmin, request, queryset: QuerySet[User]):
     description=_("Set selected users as a Qr Code Setter"),
 )
 def set_as_location_setter(modeladmin, request, queryset: QuerySet[User]):
-    for user in queryset:
-        user.groups.add(
-            Group.objects.get_or_create(name=settings.LOCATION_SETTERS_NAME)
-        )
+    set_group_membership(
+        modeladmin, request, queryset, settings.LOCATION_SETTERS_NAME, add=True
+    )
 
 
 @admin.action(
@@ -43,5 +63,6 @@ def set_as_location_setter(modeladmin, request, queryset: QuerySet[User]):
     description=_("Remove selected users as a Qr Code Setter"),
 )
 def remove_as_location_setter(modeladmin, request, queryset: QuerySet[User]):
-    for user in queryset:
-        user.groups.remove(Group.objects.get(name=settings.LOCATION_SETTERS_NAME))
+    set_group_membership(
+        modeladmin, request, queryset, settings.LOCATION_SETTERS_NAME, add=False
+    )
